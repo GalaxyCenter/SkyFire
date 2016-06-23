4c44351b74c2b46f29440a673d5dfdc7edbd8761
kuibobo <kuibobo@gmail.com>
2016-06-23 18:05:16 +0800

    实现 底部 Toolbar 栏

---
+++ app/src/main/java/apollo/tianya/emoji/ToolbarFragment.java
@@ -0,0 +1,13 @@
+package apollo.tianya.emoji;
+
+import apollo.tianya.base.BaseFragment;
+
+/**
+ * Created by Texel on 2016/6/23.
+ */
+public class ToolbarFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
