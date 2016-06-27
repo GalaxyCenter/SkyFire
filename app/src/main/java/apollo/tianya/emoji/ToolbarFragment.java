492b7ed14c28dcf311685369c47cc2ccb83b1b68
kuibobo <kuibobo@gmail.com>
2016-06-27 20:19:51 +0800

    详情页添加底部内容
    #2

--- app/src/main/java/apollo/tianya/emoji/ToolbarFragment.java
+++ app/src/main/java/apollo/tianya/emoji/ToolbarFragment.java
@@ -1,13 +1,22 @@
 package apollo.tianya.emoji;
 
+import android.view.View;
+
+import apollo.tianya.R;
 import apollo.tianya.base.BaseFragment;
+import butterknife.BindView;
 
 /**
  * Created by Texel on 2016/6/23.
  */
 public class ToolbarFragment extends BaseFragment {
+
+    public enum ToolAction {
+        ACTION_CHANGE, ACTION_WRITE_COMMENT, ACTION_VIEW_COMMENT, ACTION_FAVORITE, ACTION_SHARE, ACTION_REPORT
+    }
+
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.fragment_detail_tool_bar;
     }
 }
--- app/src/main/java/apollo/tianya/ui/DetailActivity.java
+++ app/src/main/java/apollo/tianya/ui/DetailActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import apollo.tianya.R;
 import apollo.tianya.base.BaseActivity;
 import apollo.tianya.base.BaseFragment;
+import apollo.tianya.emoji.ToolbarFragment;
 import apollo.tianya.fragment.ThreadDetailFragment;
 
 /**
@@ -13,6 +14,8 @@ import apollo.tianya.fragment.ThreadDetailFragment;
  * Created by Texel on 2016/6/20.
  */
 public class DetailActivity extends BaseActivity {
+
+    private ToolbarFragment toolFragment = new ToolbarFragment();
 
     @Override
     protected void init(Bundle savedInstanceState) {
@@ -32,6 +35,7 @@ public class DetailActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.emoji_keyboard, toolFragment).commit();
     }
 }
