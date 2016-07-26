69adb0d932c7915973af20eb23cde65353f5f4ff
kuibobo <kuibobo@163.com>
2016-07-26 22:12:50 +0800

    修改Tab显示

--- app/src/main/java/apollo/tianya/fragment/BookMarksFragment.java
+++ app/src/main/java/apollo/tianya/fragment/BookMarksFragment.java
@@ -1,7 +1,28 @@
 package apollo.tianya.fragment;
 
+import apollo.tianya.R;
+import apollo.tianya.api.TianyaParser;
+import apollo.tianya.api.remote.TianyaApi;
+import apollo.tianya.bean.DataSet;
+import apollo.tianya.bean.Thread;
+
 /**
  * Created by Texel on 2016/7/15.
  */
 public class BookMarksFragment extends ChannelFragment {
+
+    @Override
+    protected DataSet<Thread> parseList(byte[] datas) {
+        DataSet<Thread> dataset = null;
+        String body = null;
+
+        body = new String(datas);
+        dataset = TianyaParser.parseBookmarks(body);
+
+        return dataset;
+    }
+    @Override
+    protected void sendRequestData() {
+        TianyaApi.getBookMarks(mPageIndex, mHandler);
+    }
 }
